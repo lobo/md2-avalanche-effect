8aa25df870038056885d4af3d15812df8afced06
Daniel Lobo <dlobo@itba.edu.ar>
2017-10-11 02:36:06 +0200

    further changes

--- md2.py
+++ md2.py
@@ -9,22 +9,21 @@ def hamming_distance(s1, s2):
     return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
 
 def decimal_to_binary(num):
-    ''' Funcion que convierte un decimal en su valor binario '''
-    return num == 0 ? "" : decimal_to_binary(num//2) + str(num % 2) 
+    if num == 0:
+        return "" 
+    return decimal_to_binary(num//2) + str(num % 2) 
 
 def binary_to_decimal(binary):
-    ''' funcion que convierte un binario en su valor decimal '''
     decimal = 0
     cont = 0
-    print "binary: "+(binary)
+    print "binary: " + (binary)
     return 0
-    binario = str(binary)[::-1];
-    print binario
-    for digit in binario:
+    binary_number = str(binary)[::-1];
+    print binary_number
+    for digit in binary_number:
         decimal += (2**int(cont))*int(digit)
         cont +=1
     return decimal
-
 
 # ------------------------ Main Program -----------------------
 def main_program():
@@ -40,57 +39,62 @@ def main_program():
     assert '03d9963e05a094593190b6fc794cb1a3e1ac7d7883f0b5855268afeccc70d461' == s.hexdigest()
     '''
 
+    TRUE = 1
+    FALSE = 0
     SalHam = 0
-    SalData = 1
-    SalValores = 0
+    data_output_enabled = TRUE
+    stats_output_enabled = TRUE
     iterations = 10000          
     minimum_num = 64 # FIPS 180-4 SHS 08/2015
     beginning = 2**5          
     end = beginning + iterations 
     output = ""
     for i in range(beginning,end):
-        if SalData == 1 : print 50*'-'
-        data_1   = decimal_to_binary(i-1)
-        data_2   = decimal_to_binary(i)
-        if SalData == 1 : print "data_1: "+str(data_1)
-        if SalData == 1 : print "data_2: "+str(data_2)
-        value_1  = str((minimum_num-len(data_1))*"0"+data_1)
-        value_2  = str((minimum_num-len(data_2))*"0"+data_2)
-        if SalData == 1 : print "value_1: "+str(value_1)
-        if SalData == 1 : print "value_2: "+str(value_2) 
+        if data_output_enabled == 1 : print 50*'-'
+        data_1 = decimal_to_binary(i-1)
+        data_2 = decimal_to_binary(i)
+        if data_output_enabled == 1 : print "data_1: " + str(data_1)
+        if data_output_enabled == 1 : print "data_2: " + str(data_2)
+        value_1 = str((minimum_num-len(data_1))*"0" + data_1)
+        value_2 = str((minimum_num-len(data_2))*"0" + data_2)
+        if data_output_enabled == 1 : print "value_1: " + str(value_1)
+        if data_output_enabled == 1 : print "value_2: " + str(value_2) 
 
-        str0 = MD2.new(value_1).hexdigest()
-        str1 = MD2.new(value_2).hexdigest()
-        if SalData == 1 : print "str0: "+str(str0)
-        if SalData == 1 : print "str1: "+str(str1)
-        if SalData == 1 : print "Distancia de Hamming: "+str(hamming_distance(str0,str1))
-        output  +=str(hamming_distance(str0,str1))+";"
+        md2_hash_value_1 = MD2.new(value_1).hexdigest()
+        md2_hash_value_2 = MD2.new(value_2).hexdigest()
+        if data_output_enabled == 1 : print "MD2 hash for value_1: " + str(md2_hash_value_1)
+        if data_output_enabled == 1 : print "MD2 hash for value_2: " + str(md2_hash_value_2)
+        if data_output_enabled == 1 : print "Hamming Distance is: " + str(hamming_distance(md2_hash_value_1,md2_hash_value_2))
+        output += str(hamming_distance(md2_hash_value_1,md2_hash_value_2))+";"
         iterations -= 1
     if SalHam == 1 : print output
 
     SalHamNum = map(int, output.split(';')[:-1])
-    if SalValores == 1 : print 50*"-"
-    
-    #promedio
-    media = sum(SalHamNum)/float(len(SalHamNum))
-    if SalValores == 1 : print "media= "+ str(media)
+    if stats_output_enabled == 1 : print 50*"-"
 
-    # moda                                                                                   
-    repeticiones = 0                                                                         
+
+    # ------------------------ Stats -----------------------
+    
+    # Promedio
+    media = sum(SalHamNum)/float(len(SalHamNum))
+    if stats_output_enabled == 1 : print "Media: " + str(media)
+
+    # Moda                                                                                   
+    iterations = 0                                                                         
     for i in SalHamNum:                                                                              
-       apariciones = SalHamNum.count(i)                                                             
-       if apariciones > repeticiones:                                                       
-           repeticiones = apariciones                                                       
+       appearances = SalHamNum.count(i)                                                             
+       if appearances > iterations:                                                       
+           iterations = appearances                                                       
                                                                                          
     modas = []                                                                               
     for i in SalHamNum:                                                                              
-        apariciones = SalHamNum.count(i)                                                             
-        if apariciones == repeticiones and i not in modas:                                   
+        appearances = SalHamNum.count(i)                                                             
+        if appearances == iterations and i not in modas:                                   
             modas.append(i)                                                                  
                                                                                          
-    if SalValores == 1 : print "moda:", modas                                                                     
+    if stats_output_enabled == 1 : print "Moda: ", modas                                                                     
                                                                                          
-    # mediana                                                                                
+    # Mediana                                                                                
     SalHamNum.sort()                                                                                                                                                                 
                                                                                          
     if len(SalHamNum) % 2 == 0:                                                                      
@@ -99,18 +103,18 @@ def main_program():
     else:                                                                                    
         mediana =SalHamNum[len(SalHamNum)/2]                                                                 
                                                                                          
-    if SalValores == 1 : print 'mediana:',mediana 
+    if stats_output_enabled == 1 : print 'Mediana: ',mediana 
 
-    #varianza
+    # Varianza
     suma = 0
     for i in SalHamNum:
         suma += (media - float(i))**2
     varianza = float(suma)/len(SalHamNum) 
-    if SalValores == 1 : print 'varianza:',varianza
+    if stats_output_enabled == 1 : print 'Varianza: ',varianza
 
-    # desviacion estandar
-    desEstand = math.sqrt(varianza)
-    if SalValores == 1 : print 'Desviacion estandar:',desEstand
+    # Desviacion estandar
+    desviacion_estandar = math.sqrt(varianza)
+    if stats_output_enabled == 1 : print 'Desviacion estandar: ',desviacion_estandar
 
 
 main_program()
