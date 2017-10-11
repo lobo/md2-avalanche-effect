da98e872650c84b4520f8436c8560a678e4a00ad
Daniel Lobo <dlobo@itba.edu.ar>
2017-10-11 02:52:47 +0200

    more refactoring

--- md2.py
+++ md2.py
@@ -39,45 +39,39 @@ def main_program():
     assert '03d9963e05a094593190b6fc794cb1a3e1ac7d7883f0b5855268afeccc70d461' == s.hexdigest()
     '''
 
-    TRUE = 1
-    FALSE = 0
-    SalHam = 0
-    data_output_enabled = TRUE
-    stats_output_enabled = TRUE
     iterations = 10000          
     minimum_num = 64 # FIPS 180-4 SHS 08/2015
     beginning = 2**5          
     end = beginning + iterations 
     output = ""
     for i in range(beginning,end):
-        if data_output_enabled == 1 : print 50*'-'
+        print 50*'-'
         data_1 = decimal_to_binary(i-1)
         data_2 = decimal_to_binary(i)
-        if data_output_enabled == 1 : print "data_1: " + str(data_1)
-        if data_output_enabled == 1 : print "data_2: " + str(data_2)
+        print "data_1: " + str(data_1)
+        print "data_2: " + str(data_2)
         value_1 = str((minimum_num-len(data_1))*"0" + data_1)
         value_2 = str((minimum_num-len(data_2))*"0" + data_2)
-        if data_output_enabled == 1 : print "value_1: " + str(value_1)
-        if data_output_enabled == 1 : print "value_2: " + str(value_2) 
+        print "value_1: " + str(value_1)
+        print "value_2: " + str(value_2) 
 
         md2_hash_value_1 = MD2.new(value_1).hexdigest()
         md2_hash_value_2 = MD2.new(value_2).hexdigest()
-        if data_output_enabled == 1 : print "MD2 hash for value_1: " + str(md2_hash_value_1)
-        if data_output_enabled == 1 : print "MD2 hash for value_2: " + str(md2_hash_value_2)
-        if data_output_enabled == 1 : print "Hamming Distance is: " + str(hamming_distance(md2_hash_value_1,md2_hash_value_2))
+        print "MD2 hash for value_1: " + str(md2_hash_value_1)
+        print "MD2 hash for value_2: " + str(md2_hash_value_2)
+        print "Hamming Distance is: " + str(hamming_distance(md2_hash_value_1,md2_hash_value_2))
         output += str(hamming_distance(md2_hash_value_1,md2_hash_value_2))+";"
         iterations -= 1
-    if SalHam == 1 : print output
 
     SalHamNum = map(int, output.split(';')[:-1])
-    if stats_output_enabled == 1 : print 50*"-"
+    print 50*"-"
 
 
     # ------------------------ Stats -----------------------
     
     # Promedio
     media = sum(SalHamNum)/float(len(SalHamNum))
-    if stats_output_enabled == 1 : print "Media: " + str(media)
+    print "Media: " + str(media)
 
     # Moda                                                                                   
     iterations = 0                                                                         
@@ -92,7 +86,7 @@ def main_program():
         if appearances == iterations and i not in modas:                                   
             modas.append(i)                                                                  
                                                                                          
-    if stats_output_enabled == 1 : print "Moda: ", modas                                                                     
+    print "Moda: ", modas                                                                     
                                                                                          
     # Mediana                                                                                
     SalHamNum.sort()                                                                                                                                                                 
@@ -103,18 +97,18 @@ def main_program():
     else:                                                                                    
         mediana =SalHamNum[len(SalHamNum)/2]                                                                 
                                                                                          
-    if stats_output_enabled == 1 : print 'Mediana: ',mediana 
+    print 'Mediana: ',mediana 
 
     # Varianza
     suma = 0
     for i in SalHamNum:
         suma += (media - float(i))**2
     varianza = float(suma)/len(SalHamNum) 
-    if stats_output_enabled == 1 : print 'Varianza: ',varianza
+    print 'Varianza: ',varianza
 
     # Desviacion estandar
     desviacion_estandar = math.sqrt(varianza)
-    if stats_output_enabled == 1 : print 'Desviacion estandar: ',desviacion_estandar
+    print 'Desviacion estandar: ',desviacion_estandar
 
 
 main_program()
